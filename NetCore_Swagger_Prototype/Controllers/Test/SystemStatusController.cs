203433245c03709cc7fe0dbcdf4bf2f340c36f67
SyuanTsai <carsun00@Gmail.com>
2020-10-13 16:18:08 +0800

    feat:  檢驗Router
    Body:
     新建SystemController
     提供基本測試
    
    Footer:

---
+++ NetCore_Swagger_Prototype/Controllers/Test/SystemStatusController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetCore_Swagger_Prototype.Controllers.Test
+{
+    [Route("api/SystemStatus/[controller]")]
+    [ApiController]
+    public class SystemStatusController : ControllerBase
+    {
+        /// <summary>
+        ///     Supply clinet Test Server Status
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public string ApiServericStatus()
+        {
+            return "Server is Working.";
+        }
+    }
+}
---
+++ NetCore_Swagger_Prototype/Models/Prototype/Test/ReponseSystemStatus.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace NetCore_Swagger_Prototype.Models.Prototype.Test
+{
+    /// <summary>
+    ///     Repronse Api Service Test.
+    ///     回應API服務的測試.
+    ///     
+    ///     Range: DB connection, API Servers Test.
+    ///     範圍：DB連線、API服務測試。
+    ///     
+    /// </summary>
+    public class ReponseSystemStatus
+    {
+        /// <summary>
+        ///     Test function name.
+        ///     測試的相關功能名稱
+        /// </summary>
+        public string APIName { get; set; }
+
+        /// <summary>
+        ///     Execution time.
+        ///     執行時間
+        /// </summary>
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        ///     Test Result.
+        ///     測試結果
+        /// </summary>
+        public string Message { get; set; }
+    }
+}
