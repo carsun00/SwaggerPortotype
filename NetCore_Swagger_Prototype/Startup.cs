54a8c640f13c789d2de431365144acbcab9cd5a8
SyuanTsai <carsun00@Gmail.com>
2020-10-13 16:24:18 +0800

    feat:  增加Swagger
    Body:
     導入套件
     新增相關服務
    
    Footer:

--- NetCore_Swagger_Prototype/Startup.cs
+++ NetCore_Swagger_Prototype/Startup.cs
@@ -30,11 +30,19 @@ namespace NetCore_Swagger_Prototype
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
             if(env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
