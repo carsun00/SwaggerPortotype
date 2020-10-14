0259bfb8b3b61cfe51df88f06cccf32f3853b5b9
SyuanTsai <carsun00@Gmail.com>
2020-10-14 16:13:57 +0800

    feat:  Model定義 Body:  定義Model的預設值 Footer:

--- NetCore_Swagger_Prototype/Models/DTO/Demo/DemoCrudDto.cs
+++ NetCore_Swagger_Prototype/Models/DTO/Demo/DemoCrudDto.cs
@@ -1,9 +1,38 @@
-﻿namespace NetCore_Swagger_Prototype.Models.DTO.Demo
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NetCore_Swagger_Prototype.Models.DTO.Demo
 {
     public class DemoCrudDto
     {
+        /// <summary>
+        ///     編碼
+        /// </summary>
+        [Required]
         public long Id { get; set; }
+
+        /// <summary>
+        ///     姓名
+        /// </summary>
+        [StringLength(100)]
         public string Name { get; set; }
+
+        /// <summary>
+        ///     郵件
+        /// </summary>
+        [EmailAddress]
+        public string Email { get; set; }
+
+        /// <summary>
+        ///     年齡
+        /// </summary>
+        [Range(1, 100)]
+        public int Age { get; set; }
+
+        /// <summary>
+        ///     完成
+        /// </summary>
+        [DefaultValue(false)]
         public bool IsComplete { get; set; }
     }
+}
 }
